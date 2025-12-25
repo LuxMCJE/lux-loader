258ffaa14361c0409f310c7143be15ed5651e96e
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-25 13:49:49 +0100

    Update LuxLoader.java

--- src/main/java/net/luxmcje/loader/impl/LuxLoader.java
+++ src/main/java/net/luxmcje/loader/impl/LuxLoader.java
@@ -1,10 +1,13 @@
 package net.luxmcje.loader.impl;
 
-import net.luxmcje.loader.mod.ModResolver;
+import net.luxmcje.loader.impl.discovery.ModScanner;
+import net.luxmcje.loader.impl.discovery.ModCandidate;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.List;
 
 public class LuxLoader {
     public static final String VERSION = "0.1.0-alpha";
-    
     public static final String META_BASE_URL = "https://raw.githubusercontent.com/LuxMCJE/lux-meta/main/v1/";
 
     public static void boot() {
@@ -15,12 +18,28 @@ public class LuxLoader {
         System.out.println("---------------------------------------");
         
         System.out.println("[Lux-Core] Meta Server: " + META_BASE_URL);
-        System.out.println("[Lux-Core] Checking Loader Info: " + getMetaUrl("loader.json"));
+        
+        scanLocalMods();
 
+        System.out.println("[Lux-Core] Checking Loader Info: " + getMetaUrl("loader.json"));
+    }
+
+    private static void scanLocalMods() {
+        System.out.println("[Lux-Scanner] Searching for mods in /mods directory...");
+        Path modsPath = Paths.get("mods");
+        ModScanner scanner = new ModScanner();
+        
         try {
-            ModResolver.resolve();
+            scanner.scanDirectory(modsPath);
+            List<ModCandidate> candidates = scanner.getFoundMods();
+            
+            System.out.println("[Lux-Scanner] Success! Found " + candidates.size() + " mods.");
+            
+            for (ModCandidate mod : candidates) {
+                System.out.println("  >> Loaded: " + mod.id() + " [" + mod.version() + "]");
+            }
         } catch (Exception e) {
-            System.err.println("[Lux-Core] Critical error during mod resolution!");
+            System.err.println("[Lux-Scanner] Critical error while scanning mods!");
             e.printStackTrace();
         }
     }
